3dd2cc1e83affa6b654542728b3be39c9f35ffe3
hardlife0 <hellosy76@naver.com>
2024-05-01 22:30:30 +0900

    24.05.01

---
+++ 프로그래머스/짝지어제거하기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    const stack = [];
+  
+    for (let i = 0; i < s.length; i++) {
+      const curr = s.charAt(i);
+      if (stack[stack.length - 1] === curr) {
+        stack.pop();
+      } else {
+        stack.push(curr);
+      }
+    }
+  
+    return stack.length === 0 ? 1 : 0;
+  }
+  //https://doong-yi-nuna.tistory.com/50
